c79a8a6fa331d8fdb6b2a3042d18152a1a8e36f1
ismaellunkes <lunkes86@gmail.com>
2020-09-18 10:31:58 -0300

    Criando pagina Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
-import Hello from './Hello';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>      
       <Header/>  
+      <Home/>
     </div>
   );
 }
