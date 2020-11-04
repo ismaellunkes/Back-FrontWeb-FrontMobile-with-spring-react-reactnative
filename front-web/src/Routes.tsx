0044476b2a61ab9cc9bbcdebf8cb823a15ae94dc
ismaellunkes <lunkes86@gmail.com>
2020-11-04 19:55:22 -0300

    Configurando deploy front-web 5

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from './components/header';
+import Header from './components/Header';
 import Charts from './pages/Charts';
 import Home from './pages/Home';
 import Records from './pages/Records';
@@ -13,7 +13,7 @@ const Routes = () => (
                 <Home />
             </Route>
             <Route path="/records">
-                <Records/>
+                <Records />
             </Route>
             <Route path="/charts">
                 <Charts />
